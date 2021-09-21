0e69444886b212cc45f75afa50bf6876536a2c0c
Ka Wing Tsang <kawingt2@gmail.com>
2021-09-21 17:46:57 +0800

    passing data between screens

--- screen/home.js
+++ screen/home.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 export default function Home({ navigation }) {
@@ -7,10 +7,34 @@ export default function Home({ navigation }) {
 		navigation.navigate('Reviews');
 		// navigation.push('Reviews');
 	};
+	const [reviews, setReviews] = useState([
+		{
+			title: 'Zelda, Breath of Fresh Air',
+			rating: 5,
+			body: 'lorem ipsum',
+			key: '1'
+		},
+		{ title: 'Arknight', rating: 5, body: 'lorem ipsum lorum', key: '2' },
+		{
+			title: 'Fate Grand Order',
+			rating: 5,
+			body: 'lorem ipsum lorum',
+			key: '3'
+		}
+	]);
 	return (
 		<View style={globalStyles.container}>
-			<Text style={globalStyles.titleText}>Home Screen</Text>
-			<Button title='go to review' onPress={pressHandler} />
+			<Text style={globalStyles.titleText}>Home</Text>
+			<FlatList
+				data={reviews}
+				renderItem={({ item }) => (
+					<TouchableOpacity
+						onPress={() => navigation.navigate('Reviews', item)}
+					>
+						<Text style={globalStyles.titleText}>{item.title}</Text>
+					</TouchableOpacity>
+				)}
+			/>
 		</View>
 	);
 }
--- screen/reviewDetails.js
+++ screen/reviewDetails.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails({ navigation }) {
+export default function ReviewDetails({ navigation, route }) {
 	const pressHandler = () => {
 		navigation.goBack();
 	};
 
 	return (
 		<View style={globalStyles.container}>
-			<Text>Review Screen</Text>
+			<Text>{route.params.title}</Text>
+			<Text>{route.params.body}</Text>
+			<Text>{route.params.rating}</Text>
 			<Button title='back to home' onPress={pressHandler} />
 		</View>
 	);
